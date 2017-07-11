62ec880da1580fc7806b6b14a72658c5493ed91e
Unknown <matl1995@correo.ugr.es>
2017-07-11 18:34:36 +0200

    Creada main activity y botones añadidos

--- Museo/app/src/main/java/com/example/m_ang/museo/MainActivity.java
+++ Museo/app/src/main/java/com/example/m_ang/museo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.m_ang.museo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button NFCButton = (Button) findViewById(R.id.nfc_button);
+        NFCButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Click event trigger here
+            }
+        });
+
+        Button QRButton = (Button) findViewById(R.id.qr_button);
+        QRButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Click event trigger here
+            }
+        });
     }
 }
