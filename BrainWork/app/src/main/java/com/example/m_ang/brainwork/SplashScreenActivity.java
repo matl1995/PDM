92bcbe3a4519d5aa06834b103e8f0a9c059f19b0
Unknown <matl1995@correo.ugr.es>
2017-08-10 13:17:36 +0200

    Inicio app BrainWork
    Creada Splash Screen y menu para elegir actividad

---
+++ BrainWork/app/src/main/java/com/example/m_ang/brainwork/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.example.m_ang.brainwork;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 4500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, InicioActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
